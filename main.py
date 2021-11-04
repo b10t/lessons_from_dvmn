e4e919b0638b9e379c60e36b0cd6e62da4ae28a7
b10t <b10t.rus@gmail.com>
2021-11-05 03:05:57 +0800

    Lesson 2

--- main.py
+++ main.py
@@ -1,15 +1,58 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-payload = {"n": "",
-           "T": "",
-           "q": "",
-           "m": "",
-           "lang": "ru"}
+load_dotenv()
 
-places = ['Лондон', 'Шереметьево', 'Череповец']
+token = os.getenv('TOKEN')
 
-for place in places:
-  response = requests.get(f'https://wttr.in/{place}', params=payload)
-  response.raise_for_status()
 
-  print(response.text)
+def shorten_link(token, url):
+    headers = {'Authorization': f'Bearer {token}'}
+    payload = {'long_url': url}
+
+    response = requests.post(
+        f'https://api-ssl.bitly.com/v4/bitlinks',
+        headers=headers,
+        json=payload)
+
+    response.raise_for_status()
+
+    return response.json()['id']
+
+
+def count_clicks(token, bitLink):
+    headers = {'Authorization': f'Bearer {token}'}
+    payload = {'units': -1}
+
+    response = requests.get(
+        f'https://api-ssl.bitly.com/v4/bitlinks/{bitLink}/clicks/summary',
+        headers=headers,
+        params=payload)
+
+    response.raise_for_status()
+
+    return response.json()['total_clicks']
+
+
+def is_bitlink(token, url):
+    headers = {'Authorization': f'Bearer {token}'}
+
+    response = requests.get(
+        f'https://api-ssl.bitly.com/v4/bitlinks/{url}',
+        headers=headers)
+
+    return response.ok
+
+
+if __name__ == '__main__':
+    try:
+        user_input = input('Введите ссылку: ')
+
+        if is_bitlink(token, user_input):
+            print('Кол-во кликов', count_clicks(token, user_input))
+        else:
+            bitLink = shorten_link(token, user_input)
+            print('Битлинк', bitLink)
+    except requests.exceptions.HTTPError:
+        print('Error')
