8aafae3fe361677594ffbb15c3ef76d21e5ec1f6
b10t <b10t.rus@gmail.com>
2021-11-05 18:56:55 +0800

    Lesson 2 fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+from urllib.parse import urlparse, urlunparse
 
 load_dotenv()
 
@@ -8,6 +9,15 @@ token = os.getenv('BITLY_TOKEN')
 
 
 def shorten_link(token, url):
+    """Сокращяет ссылку через интерфейс bit.ly
+
+    Args:
+        token (str): TOKEN API Bitly
+        url (str): Ссылка
+
+    Returns:
+        str: Сокращённая ссылка
+    """
     headers = {'Authorization': f'Bearer {token}'}
     payload = {'long_url': url}
 
@@ -18,15 +28,24 @@ def shorten_link(token, url):
 
     response.raise_for_status()
 
-    return response.json()['id']
+    return response.json()['link']
 
 
-def count_clicks(token, bitLink):
+def count_clicks(token, bitly_link):
+    """Возвращает количество кликов по ссылке
+
+    Args:
+        token (str): TOKEN API Bitly
+        bitly_link (str): Сокращённая ссылка
+
+    Returns:
+        int: Количество кликов по ссылке
+    """
     headers = {'Authorization': f'Bearer {token}'}
     payload = {'units': -1}
 
     response = requests.get(
-        f'https://api-ssl.bitly.com/v4/bitlinks/{bitLink}/clicks/summary',
+        f'https://api-ssl.bitly.com/v4/bitlinks/{bitly_link}/clicks/summary',
         headers=headers,
         params=payload)
 
@@ -46,13 +65,28 @@ def is_bitlink(token, url):
 
 
 if __name__ == '__main__':
-    try:
-        user_input = input('Введите ссылку: ')
+    url = input('Введите ссылку: ')
+    url = urlparse(url)
+    url_scheme = '%s://' % url.scheme
 
-        if is_bitlink(token, user_input):
-            print('Кол-во кликов', count_clicks(token, user_input))
-        else:
-            bitLink = shorten_link(token, user_input)
-            print('Битлинк', bitLink)
-    except requests.exceptions.HTTPError:
-        print('Error')
+    if len(url.scheme) == 0:
+        print('Введите ссылку полность, со схемой (http://... или https://...)')
+        exit(1)
+
+    # Удаляем схему для проверки типа ссылки
+    url = url.geturl().replace(url_scheme, '', 1)
+
+    if is_bitlink(token, url):
+        try:
+            print('Кол-во кликов', count_clicks(token, url))
+        except requests.exceptions.HTTPError:
+            print('Ошибка при получении количества кликов.')
+            exit(1)
+    else:
+        # Добавляем схему к адресу
+        url = url_scheme + url
+
+        try:
+            print('Битлинк', shorten_link(token, url))
+        except:
+            print('Ошибка при получении короткой ссылки.')
