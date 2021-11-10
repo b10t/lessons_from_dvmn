804794faf0ea62917b9be023fe3ff4361795f750
b10t <b10t.rus@gmail.com>
2021-11-10 10:23:08 +0800

    Высушил код.

--- main.py
+++ main.py
@@ -7,107 +7,8 @@ from datetime import datetime
 import telegram
 import random
 import time
-
-
-def download_image(url, path):
-    """Скачивает изображение по ссылке и сохраняет в указаную папку
-
-    Args:
-        url (str): Ссылка на изображение
-        path (str): Путь до папке где сохранить изображение
-    """
-    response = requests.get(url)
-    response.raise_for_status()
-
-    with open(path, 'wb') as f:
-        f.write(response.content)
-
-
-def get_file_name_from_url(url):
-    """Получает имя файла из url
-
-    Args:
-        url (str): Ссылка на файл
-
-    Returns:
-        str: Расширение файла
-    """
-    return os.path.basename(urlsplit(url).path)
-
-
-def get_extension_from_url(url):
-    """Получает расширение файла из url
-
-    Args:
-        url (str): Ссылка на файл
-
-    Returns:
-        str: Расширение файла
-    """
-    return os.path.splitext(urlsplit(url).path)[1]
-
-
-def fetch_nasa_apod_images(token):
-    """Загружает картинки через API NASA"""
-    payload = {'api_key': token,
-               'count': 30}
-    response = requests.get(
-        'https://api.nasa.gov/planetary/apod', params=payload)
-    response.raise_for_status()
-
-    for json in response.json():
-        url_image = json['url']
-
-        if urlsplit(url_image).netloc == 'apod.nasa.gov':
-            download_image(
-                url_image, f'./images/{get_file_name_from_url(json["url"])}')
-
-
-def fetch_nasa_epic_images(token):
-    """Загружает картинки через API NASA"""
-    payload = {'api_key': token}
-
-    response = requests.get(
-        'https://api.nasa.gov/EPIC/api/natural', params=payload)
-    response.raise_for_status()
-
-    for json in response.json():
-        url_image = fetch_nasa_epic_url_image(token, json)
-
-        if urlsplit(url_image).netloc == 'api.nasa.gov':
-            download_image(
-                url_image, f'./images/{json["image"]}{get_extension_from_url(url_image)}')
-
-
-def fetch_nasa_epic_url_image(token, response_json):
-    """Получает ссылку на изображение
-
-    Args:
-        token (str): TOKEN API NASA
-        response_json (json): Содержит структуру по картинке
-
-    Returns:
-        str: Ссылка на изображение
-    """
-    date_image = datetime.fromisoformat(response_json['date'])
-
-    return f'https://api.nasa.gov/EPIC/archive/natural/{date_image.strftime("%Y/%m/%d")}/png/{response_json["image"]}.png?api_key={token}'
-
-
-def fetch_spacex_last_launch():
-    """Загружает картинки через API SpaceX"""
-    response = requests.get('https://api.spacexdata.com/v4/launches')
-
-    for i in response.json():
-        if i['links']['flickr']['original']:
-            for index, url in enumerate(i['links']['flickr']['original'], 1):
-                response = requests.get(url)
-                response.raise_for_status()
-
-                download_image(
-                    url,
-                    './images/spacex%s.jpg' % index)
-            break
+from fetch_spacex import fetch_spacex_last_launch
+from fetch_nasa import fetch_nasa_apod_images, fetch_nasa_epic_images
 
 
 if __name__ == '__main__':
@@ -117,6 +18,7 @@ if __name__ == '__main__':
     load_dotenv()
     nasa_token = os.getenv('NASA_TOKEN')
     telegram_token = os.getenv('TELEGRAM_TOKEN')
+    id_telegram_channel = os.getenv('ID_TELEGRAM_CHANNEL')
     timeout = int(os.getenv('TIMEOUT', 86400))
 
     while True:
@@ -128,7 +30,7 @@ if __name__ == '__main__':
 
         for root, dirs, files in os.walk(path_to_image):
             bot.send_photo(
-                chat_id='@photos_from_nasa',
+                chat_id=id_telegram_channel,
                 photo=open(f'{path_to_image}{files[random.randint(0, len(files))]}', 'rb'))
 
         time.sleep(timeout)
