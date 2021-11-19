31af08133772dbe0fcb99070f2bfae697ce383ab
b10t <b10t.rus@gmail.com>
2021-11-19 15:32:03 +0800

    Добавлено автоматическое создание папки для изображений.

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,5 +1,6 @@
 import os
 import time
+from pathlib import Path
 
 import requests
 from dotenv import load_dotenv
@@ -30,6 +31,8 @@ if __name__ == '__main__':
     load_dotenv()
     path_to_images = os.getenv('PATH_TO_IMAGES', './images/')
     timeout = int(os.getenv('TIMEOUT', 86400))
+
+    Path(path_to_images).mkdir(parents=True, exist_ok=True)
 
     while True:
         fetch_spacex_last_launch(path_to_images)
